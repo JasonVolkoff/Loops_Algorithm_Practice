205b6ebf62bcaa64e7cefed13759a1e9e91a28a8
JasonVolkoff <jasonvolkoff@mail.fresnostate.edu>
2021-03-08 14:05:40 -0800

    Loops algorithm practice.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Print out all odd numbers from 1 to 20 //
+for (var i = 1; i < 21; i += 2) {
+    console.log(i);
+}
+
+
+// Sum numbers from 1 to 5, printing out the current number and sum so far at each step of the way //
+var sum = 0;
+for (var i = 0; i < 6; i++) {
+    sum = sum + i;
+    console.log("Num: ", i, ", Sum: ", sum);
+}
